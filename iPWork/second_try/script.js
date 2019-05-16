46f5ce89a40ed73d0bd55833d9c780de69260a23
Jeremy Owens <jimcowens@gmail.com>
2019-05-16 13:47:32 -0400

    working on rendering a generating fibonacci sequence function

--- iPWork/second_try/script.js
+++ iPWork/second_try/script.js
@@ -1,5 +1,5 @@
 let x = document.getElementById("demo")
-
+let num=0
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(showPosition)
@@ -9,5 +9,31 @@ function getLocation() {
 }
 
 function showPosition(position) {
+  console.log(position)
   x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude
+}
+
+function* fibonacci() { 
+  let [prev, curr] = [0, 1]
+  while (true) {
+    [prev, curr] = [curr, prev + curr]
+    yield curr
+  }
+}
+
+for (let n of fibonacci()) {
+  if(num===0){
+    console.log(`the 1st number in the fibonacci sequence is: ${n}`)
+  }else if (num===1){
+    console.log(`the 2nd number in the fibonacci sequence is: ${n}`)
+  }else if(num===2){
+    console.log(`the 3rd number in the fibonacci sequence is: ${n}`)
+  }else{
+  console.log(`the ${num+2}th number in the fibonacci sequence is: ${n}`)
+  }
+  num++
+  // end the loop at 1000
+  if (num >= 100) {
+    break;
+  }
 }
